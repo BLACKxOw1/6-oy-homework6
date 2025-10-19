a327713baa25b241f270c9bf2b4932cea96ef01a
BLACKxOw1 <nuriddintuychiyev5@gmail.com>
2025-10-19 09:18:09 +0500

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
 
@@ -6,25 +5,9 @@ function App() {
 
   return (
     <div className='text-4xl text-center'>
-      hello world
+      hello nuriddin
     </div>
   )
-=======
-import React from "react";
-import "./index.css";
-import Header from "./components/Header";
-import Main from "./components/main";
-import Footer from "./components/Footer";
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
->>>>>>> origin/main
 }
 
 export default App
