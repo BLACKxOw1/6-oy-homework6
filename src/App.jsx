189329fc9511c98dbae4825a66e46d9aef065ad4
BLACKxOw1 <nuriddintuychiyev5@gmail.com>
2025-10-19 09:16:46 +0500

    Yangi loyihani qo‘shdim

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div className='text-4xl text-center'>
+      hello world
+    </div>
+  )
+}
+
+export default App
